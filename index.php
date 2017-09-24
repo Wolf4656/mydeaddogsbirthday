7cbd083ac93ab0fb00cb5b25395b8c5ffefce3b2
Wolf4656 <wolf4656@fredonia.edu>
2017-09-24 14:46:20 -0400

    09242017
    Fixed some apges

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php include 'header.php'?>
-<?php
-  header('Location: showinprogress.php')
- ?>
+<div class="show-in-progress">
+  <p>TO SUBMIT A PROPOSAL PLEASE FILL OUT THE FOLLOWING</p>
+  <p><a href="proposal.pdf">PDF DOCUMENT</a></p>
+  <p>AND SEND IT BACK TO</p>
+  <p style="color: red;">DEADDOGSBIRTHDAY@GMAIL.COM</p>
+  <p>WITH "CURATIORIAL PROPOSAL" IN THE HEADER</p>
+</div>
 <?php include 'footer.php' ?>
